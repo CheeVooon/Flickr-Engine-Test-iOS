26d7d417604434cf4cd5f350fadcaaeb1d6a219d
Chee Voon <limcheevoon@gmail.com>
2023-04-27 22:24:10 +0800

    Initial Commit

---
+++ Flickr Engine Test iOS/Flickr_Engine_Test_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_Engine_Test_iOSApp.swift
+//  Flickr Engine Test iOS
+//
+//  Created by Chee Voon on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_Engine_Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
